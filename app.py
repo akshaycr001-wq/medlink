9c97b27e9d049bb593c0ae8a34e9f005bbdcc96f
Akshay <akshaycr01@gmail.com>
2026-04-25 16:28:34 +0530

    Enable real email delivery by defaulting MAIL_DEBUG_MODE to False

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
-MAIL_DEBUG_MODE = os.environ.get('MAIL_DEBUG_MODE', 'True').lower() == 'true'
+MAIL_DEBUG_MODE = os.environ.get('MAIL_DEBUG_MODE', 'False').lower() == 'true'
 mail = Mail(app)
 
 # Initialize security extensions
